8df1d65d969f35b43bb61c14aa90c2d3139dcb82
Hugo Rodrigez <hugor37@gmail.com>
2020-08-14 21:43:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,8 @@ def nyc_pigeon_organizer(data)   # write your code here!
 data.each_with_object({}) do |(key, value), new_array|
   value.each do |subkey, names|
     names.each do |name|
+      if !new_array[name]
+        final_array[name] = {}
   end
   final_array
 end
