47a7a30f1a437c48d08a0d9f8c1493e5ef01cdb8
Hugo Rodrigez <hugor37@gmail.com>
2020-08-14 21:50:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,12 +5,13 @@ data.each_with_object({}) do |(key, value), new_array|
   value.each do |subkey, names|
     names.each do |name|
       if !new_array[name]
-        final_array[name] = {}
+        new_array[name] = {}
       end
    if !array[name][key]
-     final_array[name] = {}
+     !new_array[name] = {}
     end
+  new_array[name][key].push()
   end
-  final_array
+  new_array
 end
 end
